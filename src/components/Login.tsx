81a3a38331043d93cf1cacd78498d19b3f4b0250
Prince Nana  Mensah <burnszephyr520@gmail.com>
2025-09-11 03:55:24 +0000

    "Styles"

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -34,7 +34,7 @@ export const Login: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <div className="mx-auto h-12 w-12 flex items-center justify-center rounded-full bg-blue-100">
